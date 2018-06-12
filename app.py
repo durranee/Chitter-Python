7513eecc5bbb7a3f32b11caa500f3fa261b2f500
antcin <ciniglioantonio@gmail.com>
2018-06-12 11:16:05 +0100

    pass 404 test

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
+# db configuration
+DATABASE = 'pythonchitter.db'
+DEBUG = True
+SECRET_KEY = 'python'
+USERNAME = 'admin'
+PASSWORD = 'password'
+
+# create and initialise app
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
+app.config.from_object(__name__)
 
 if __name__ == '__main__':
     app.run()
